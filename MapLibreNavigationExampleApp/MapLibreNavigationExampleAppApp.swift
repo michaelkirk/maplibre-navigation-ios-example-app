427ba7ee0b62cefd7dc4b0f7ad0095a16b0b2c56
Michael Kirk <michael.code@endoftheworl.de>
2024-05-14 16:22:42 -0700

    Initial Commit

---
+++ MapLibreNavigationExampleApp/MapLibreNavigationExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapLibreNavigationExampleAppApp.swift
+//  MapLibreNavigationExampleApp
+//
+//  Created by Michael Kirk on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MapLibreNavigationExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
